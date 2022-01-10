6d63f819fa92988714a479b2ffa480d5b6760d5b
Byran <sunhaolin@hotoa.com>
2022-01-10 16:32:25 +0800

    发布到aws cn ECR

---
+++ steedos-base/sync-to-aws-cn.sh
@@ -0,0 +1,5 @@
+aws ecr get-login-password --region cn-northwest-1 | docker login --username AWS --password-stdin 252208178451.dkr.ecr.cn-northwest-1.amazonaws.com.cn
+
+docker tag steedos/gitpod-workspace-base:latest 252208178451.dkr.ecr.cn-northwest-1.amazonaws.com.cn/dockerhub/gitpod/workspace-base:latest
+
+docker push 252208178451.dkr.ecr.cn-northwest-1.amazonaws.com.cn/dockerhub/gitpod/workspace-base:latest
