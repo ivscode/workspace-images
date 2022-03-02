246b94f12a790b545c06a69740fb03a0eced2756
chenzhipei <18605199364@163.com>
2022-03-02 05:20:42 +0000

    add tag

--- steedos-base/sync-to-aws-cn.sh
+++ steedos-base/sync-to-aws-cn.sh
@@ -1,7 +1,11 @@
 aws ecr get-login-password --region cn-northwest-1 | docker login --username AWS --password-stdin 252208178451.dkr.ecr.cn-northwest-1.amazonaws.com.cn
 
-tag=2.1
+tag=2.1.1
 
 docker tag steedos/gitpod-workspace-base:latest 252208178451.dkr.ecr.cn-northwest-1.amazonaws.com.cn/dockerhub/steedos/gitpod-workspace-base:$tag
+docker tag steedos/gitpod-workspace-base:latest 252208178451.dkr.ecr.cn-northwest-1.amazonaws.com.cn/dockerhub/steedos/gitpod-workspace-base:2.1
+docker tag steedos/gitpod-workspace-base:latest 252208178451.dkr.ecr.cn-northwest-1.amazonaws.com.cn/dockerhub/steedos/gitpod-workspace-base:latest
 
 docker push 252208178451.dkr.ecr.cn-northwest-1.amazonaws.com.cn/dockerhub/steedos/gitpod-workspace-base:$tag
+docker push 252208178451.dkr.ecr.cn-northwest-1.amazonaws.com.cn/dockerhub/steedos/gitpod-workspace-base:2.1
+docker push 252208178451.dkr.ecr.cn-northwest-1.amazonaws.com.cn/dockerhub/steedos/gitpod-workspace-base:latest
